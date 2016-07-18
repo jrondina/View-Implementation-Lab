baf1ecf4768e6bedace280bdce22f4ed60a76d96
James <jamesrondina@gmail.com>
2016-07-18 15:43:15 -0400

    4 things done so far

---
+++ app/src/main/java/com/example/jamesrondina/layouts/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jamesrondina.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.facebook_list_item);
+    }
+}
